04f4e4a990e90bfaf847b0896e758e382e26a67b
ewbriao1978 <eduardo.briao@gmail.com>
2025-06-05 14:59:52 -0300

    comandos

--- server.js
+++ server.js
@@ -4,17 +4,71 @@
 // node --watch server.js
 
 
+// para receber dados em JSON no body
+// app.use(express.json());
 const express = require('express');
 const app = express();
+// para usar json no body
+app.use(express.json());
 
 const PORT = 3000;
+
+app.get('/nothing', (req, res) => {
+    const dados = {
+        frase: 'ERROR!!! Rota não encontrada!'
+        };
+    res.status(404).json(dados);
+})
+
+app.post('/receba', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.nome);
+
+    const dados = {
+        frase: 'Rota POST RECEBA recebida com sucesso!'
+        };
+    res.status(200).json(dados);
+})
+
+
+app.post('/calcule', (req, res) => {
+    // body: 
+    // { 
+   // salario: 10000.00
+//     } 
+    salario = Number(req.body.salario);
+    console.log(salario);
+    imposto = salario * 0.27;
+    console.log(imposto);
+    salarioLiquido = salario - imposto;
+
+     const dados = {
+         "imposto": imposto.toString(),
+         "salarioLiquido": salarioLiquido.toString(),
+         "mensagem": "Cálculo realizado com sucesso!"
+         };
+    res.status(200).json(dados);
+})
+    
+
 
 app.get('/', (req, res) => {
     const dados = {
         frase: 'Minha primeira rota com Express!'
         };
-    res.json(dados);
+    res.status(200).json(dados);
 })
+
+
+app.get('/bem_vindo', (req, res) => {
+    const msg = {
+        mensagem: 'Bem-vindo ao mundo do node!',
+    
+        };
+    res.status(200).json(msg);
+})
+
+
 
 app.get('/produtos', (req, res) => {
     const produtos = [
@@ -22,7 +76,7 @@ app.get('/produtos', (req, res) => {
         { id: 2, nome: 'Nintendo Switch 2 :(', preco: 4500.00 },
         { id: 3, nome: 'Cassio Watch', preco: 500.00 }
     ];
-    res.json(produtos);
+    res.status(200).json(produtos);
 })
 
 app.listen(PORT, () => {
