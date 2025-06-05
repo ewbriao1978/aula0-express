c9b0a8b13bc9f1f95c1af883c02459aa5759521a
ewbriao1978 <eduardo.briao@gmail.com>
2025-06-05 13:41:26 -0300

    Aula 0

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    const dados = {
+        frase: 'Minha primeira rota com Express!'
+        };
+    res.json(dados);
+})
+
+app.get('/produtos', (req, res) => {
+    const produtos = [
+        { id: 1, nome: 'Iphone', preco: 8000.00 },
+        { id: 2, nome: 'Nintendo Switch 2 :(', preco: 4500.00 },
+        { id: 3, nome: 'Cassio Watch', preco: 500.00 }
+    ];
+    res.json(produtos);
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+  
